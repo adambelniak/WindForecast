b023f32909ff3ce6fa38baef52292b94de0e2a7b
adam <adam.belniak@balticdatascience.com>
2020-08-25 13:02:34 +0200

    fix bugs

--- draft_model_run.py
+++ draft_model_run.py
@@ -5,7 +5,6 @@ from preprocess.gfs_preprocess import prepare_gfs_dataset, CREATED_AT_COLUMN_NAM
 from preprocess.synop_preprocess import prepare_synop_dataset, split_features_into_arrays
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 AUXILIARY_COLUMNS = ["10m U-wind, m/s", "10m V-wind, m/s", "Gusts, m/s"]
 
@@ -41,7 +40,7 @@ def create_sequence(data, gfs_data, past_len, future_offset):
         label = data.iloc[future_index][['velocity']].values.flatten()
         train_synop_input.append(past_synop_data.astype(np.float32))
         train_synop_label.append(label.astype(np.float32))
-        gfs_input.append(gfs_prediction)
+        gfs_input.append(gfs_prediction.astype(np.float32))
 
     return np.array(train_synop_input), np.array(gfs_input), np.array(train_synop_label)
 
@@ -50,27 +49,29 @@ def prepare_data(past_len=12, future_offset=12, train_split_factor=0.75):
     dataset_train = prepare_synop_dataset("preprocess/synop_data/135_data.csv")
     gfs_dataset = prepare_gfs_dataset("preprocess/wind_and_temp", 'date', future_offset)
 
-
     least_recent_date = gfs_dataset["date"].min()
     dataset_train = dataset_train[dataset_train["date"] >= least_recent_date - timedelta(hours=past_len)]
     gfs_dataset = gfs_dataset[gfs_dataset["date"] > least_recent_date + timedelta(hours=future_offset)]
     gfs_dataset = gfs_dataset.sort_values(by=["date"])
-    
+
     train_synop_input, gfs_input, train_synop_label = create_sequence(dataset_train, gfs_dataset, past_len,
                                                                       future_offset)
+    train_size = int(train_synop_input.shape[0] * train_split_factor)
 
-    return train_synop_input, gfs_input, train_synop_label
-    # sequence_length = int(past_len / step)
-    # train_split = int(len(dataset_train) * train_split_factor)
+    x_train = [train_synop_input[:train_size], gfs_input[:train_size]]
+    y_train = train_synop_label[:train_size]
+    x_valid = [train_synop_input[train_size:], gfs_input[train_size:]]
+    y_valid = train_synop_label[train_size:]
 
+    return x_train, y_train, x_valid, y_valid
 
 
 def run_model():
-    train_synop_input, gfs_input, train_synop_label = prepare_data()
+    x_train, y_train, x_valid, y_valid = prepare_data()
 
-    model = create_model(train_synop_input, gfs_input, 0.001)
-    history = model.fit([train_synop_input, gfs_input], [train_synop_label], epochs=300, batch_size=32)
+    model = create_model(x_train[0], x_train[1], 0.001)
+    history = model.fit(x_train, y_train, epochs=300, batch_size=32, validation_data=(x_valid, y_valid), )
 
 
 if __name__ == '__main__':
-    prepare_data()
+    run_model()
