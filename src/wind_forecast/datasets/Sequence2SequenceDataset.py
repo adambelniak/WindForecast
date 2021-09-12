Fix normalization in sequence datasets related to GFS

src/wind_forecast/datasets/Sequence2SequenceDataset.py
@@ -15,6 +15,7 @@ class Sequence2SequenceDataset(torch.utils.data.Dataset):
         self.future_sequence_length = config.experiment.future_sequence_length
         self.prediction_offset = config.experiment.prediction_offset
         self.synop_data = synop_data.reset_index()
+        # Get indices which correspond to 'dates' - 'dates' are the ones, which start a proper sequence without breaks
         synop_data_indices = self.synop_data[self.synop_data["date"].isin(dates)].index
         target_param_index = [x[1] for x in self.train_params].index(self.target_param)
 
@@ -22,7 +23,9 @@ class Sequence2SequenceDataset(torch.utils.data.Dataset):
             # data was not normalized, so take all frames which will be used, compute std and mean and normalize data
             self.synop_data, synop_mean, synop_std = normalize_synop_data(self.synop_data, synop_data_indices,
                                                                           list(list(zip(*self.train_params))[1]),
-                                                                          self.sequence_length + self.prediction_offset + self.future_sequence_length)
+                                                                          self.sequence_length + self.prediction_offset
+                                                                          + self.future_sequence_length,
+                                                                          config.experiment.normalization_type)
             print(synop_mean[target_param_index])
             print(synop_std[target_param_index])
 

src/wind_forecast/datasets/SequenceDataset.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 
 from wind_forecast.config.register import Config
 from wind_forecast.preprocess.synop.synop_preprocess import normalize_synop_data
+from wind_forecast.util.utils import add_param_to_train_params
 
 
 class SequenceDataset(torch.utils.data.Dataset):
@@ -15,13 +16,17 @@ class SequenceDataset(torch.utils.data.Dataset):
         self.sequence_length = config.experiment.sequence_length
         self.prediction_offset = config.experiment.prediction_offset
         self.synop_data = synop_data.reset_index()
+        # Get indices which correspond to 'dates' - 'dates' are the ones, which start a proper sequence without breaks
         synop_data_indices = self.synop_data[self.synop_data["date"].isin(dates)].index
+        params = add_param_to_train_params(self.train_params, self.target_param)
+        feature_names = list(list(zip(*params))[1])
         if normalize_synop:
             # data was not normalized, so take all frames which will be used, compute std and mean and normalize data
             self.synop_data, synop_mean, synop_std = normalize_synop_data(self.synop_data, synop_data_indices,
-                                                                          list(list(zip(*self.train_params))[1]),
-                                                                          self.sequence_length + self.prediction_offset)
-            target_param_index = [x[1] for x in self.train_params].index(self.target_param)
+                                                                          feature_names,
+                                                                          self.sequence_length + self.prediction_offset,
+                                                                          config.experiment.normalization_type)
+            target_param_index = [x[1] for x in feature_names].index(self.target_param)
             print(synop_mean[target_param_index])
             print(synop_std[target_param_index])
 
@@ -32,7 +37,9 @@ class SequenceDataset(torch.utils.data.Dataset):
                         for index in tqdm(synop_data_indices)]
 
         assert len(self.features) == len(self.targets)
+
         length = len(self.targets)
+
         training_data = np.array(list(zip(self.features, self.targets))[:int(length * 0.8)])
         test_data = np.array(list(zip(self.features, self.targets))[int(length * 0.8) + self.sequence_length - 1:])
         if train:

src/wind_forecast/datasets/SequenceWithGFSDataset.py
@@ -3,10 +3,10 @@ import math
 import torch
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 from wind_forecast.config.register import Config
-from wind_forecast.consts import SYNOP_DATASETS_DIRECTORY
-from wind_forecast.preprocess.synop.synop_preprocess import prepare_synop_dataset, normalize
+from wind_forecast.preprocess.synop.synop_preprocess import normalize_synop_data
 from wind_forecast.util.utils import add_param_to_train_params, match_gfs_with_synop_sequence, \
     target_param_to_gfs_name_level, NormalizationType
 
@@ -14,7 +14,7 @@ from wind_forecast.util.utils import add_param_to_train_params, match_gfs_with_s
 class SequenceWithGFSDataset(torch.utils.data.Dataset):
     """Characterizes a dataset for PyTorch"""
 
-    def __init__(self, config: Config, train=True):
+    def __init__(self, config: Config, synop_data, dates, train=True, normalize_synop=True):
         """Initialization"""
         self.target_param = config.experiment.target_parameter
         self.train_params = config.experiment.synop_train_features
@@ -22,33 +22,41 @@ class SequenceWithGFSDataset(torch.utils.data.Dataset):
         self.sequence_length = config.experiment.sequence_length
         self.prediction_offset = config.experiment.prediction_offset
         self.target_coords = config.experiment.target_coords
+        self.synop_data = synop_data.reset_index()
+        # Get indices which correspond to 'dates' - 'dates' are the ones, which start a proper sequence without breaks
+        synop_data_indices = self.synop_data[self.synop_data["date"].isin(dates)].index
 
-        params = add_param_to_train_params(self.train_params, self.target_param)
-        feature_names = list(list(zip(*params))[1])
+        all_params = add_param_to_train_params(self.train_params, self.target_param)
+        feature_names = list(list(zip(*all_params))[1])
 
-        synop_data, _, _ = prepare_synop_dataset(self.synop_file, feature_names, norm=False,
-                                                 dataset_dir=SYNOP_DATASETS_DIRECTORY, from_year=2015)
+        if normalize_synop:
+            # data was not normalized, so take all frames which will be used, compute std and mean and normalize data
+            self.synop_data, synop_feature_1, synop_feature_2 = normalize_synop_data(self.synop_data,
+                                                                                     synop_data_indices,
+                                                                                     feature_names,
+                                                                                     self.sequence_length + self.prediction_offset,
+                                                                                     config.experiment.normalization_type)
+            target_param_index = [x for x in feature_names].index(self.target_param)
+            print(synop_feature_1[target_param_index])
+            print(synop_feature_2[target_param_index])
 
-        synop_data_dates = synop_data['date']
-        # normalize here to keep the param_name <> value mapping
-        synop_data, synop_feature_1, synop_feature_2 = normalize(synop_data[feature_names], config.experiment.normalization_type)
+        synop_data_dates = self.synop_data['date']
+        # labels and dates - dates are needed for matching the labels against GFS dates
+        labels = pd.concat([synop_data_dates, self.synop_data[self.target_param]], axis=1)
 
-        labels = pd.concat([synop_data_dates, synop_data[self.target_param]], axis=1).to_numpy()
+        self.features = [
+            self.synop_data.iloc[index:index + self.sequence_length][list(list(zip(*self.train_params))[1])].to_numpy()
+            for index in tqdm(synop_data_indices)]
 
-        train_synop_data = synop_data[list(list(zip(*self.train_params))[1])].to_numpy()
+        targets = [labels.iloc[index + self.sequence_length + self.prediction_offset]
+                   for index in tqdm(synop_data_indices)]
 
-        features = [train_synop_data[i:i + self.sequence_length, :].T for i in
-                    range(train_synop_data.shape[0] - self.sequence_length - self.prediction_offset + 1)]
-
-        targets = [labels[i + self.sequence_length + self.prediction_offset - 1] for i in
-                   range(labels.shape[0] - self.sequence_length - self.prediction_offset + 1)]
-        features = np.transpose(np.array(features), (0, 2, 1))
-
-        self.features, self.gfs_data, self.targets = match_gfs_with_synop_sequence(features, targets,
+        self.features, self.gfs_data, self.targets = match_gfs_with_synop_sequence(self.features, targets,
                                                                                    self.target_coords[0],
                                                                                    self.target_coords[1],
                                                                                    self.prediction_offset,
-                                                                                   target_param_to_gfs_name_level(self.target_param))
+                                                                                   target_param_to_gfs_name_level(
+                                                                                       self.target_param))
 
         if self.target_param == "wind_velocity":
             self.gfs_data = np.array([math.sqrt(velocity[0] ** 2 + velocity[1] ** 2) for velocity in self.gfs_data])
@@ -72,9 +80,6 @@ class SequenceWithGFSDataset(torch.utils.data.Dataset):
             data = test_data
 
         self.data = data
-
-        print(synop_feature_1)
-        print(synop_feature_2)
 
     def __len__(self):
         """Denotes the total number of samples"""

src/wind_forecast/datasets/SingleGFSPointDataset.py
@@ -21,7 +21,8 @@ class SingleGFSPointDataset(torch.utils.data.Dataset):
         self.target_coords = config.experiment.target_coords
 
         synop_data, synop_mean, synop_std = prepare_synop_dataset(self.synop_file, [self.target_param],
-                                                                  dataset_dir=SYNOP_DATASETS_DIRECTORY, from_year=2015)
+                                                                  dataset_dir=SYNOP_DATASETS_DIRECTORY,
+                                                                  from_year=config.experiment.synop_from_year)
 
         synop_data_dates = synop_data['date']
         labels = pd.concat([synop_data_dates, synop_data[self.target_param]], axis=1).to_numpy().tolist()