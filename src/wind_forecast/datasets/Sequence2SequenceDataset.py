Fix normalizing data for synop sequences

src/wind_forecast/datasets/Sequence2SequenceDataset.py
@@ -1,12 +1,13 @@
 import torch
 from tqdm import tqdm
 from wind_forecast.config.register import Config
+from wind_forecast.preprocess.synop.synop_preprocess import normalize_synop_data
 
 
 class Sequence2SequenceDataset(torch.utils.data.Dataset):
     'Characterizes a dataset for PyTorch'
 
-    def __init__(self, config: Config, synop_data, dates, train=True):
+    def __init__(self, config: Config, synop_data, dates, train=True,  normalize_synop=True):
         'Initialization'
         self.target_param = config.experiment.target_parameter
         self.train_params = config.experiment.synop_train_features
@@ -16,6 +17,14 @@ class Sequence2SequenceDataset(torch.utils.data.Dataset):
         self.synop_data = synop_data.reset_index()
         synop_data_indices = self.synop_data[self.synop_data["date"].isin(dates)].index
         target_param_index = [x[1] for x in self.train_params].index(self.target_param)
+
+        if normalize_synop:
+            # data was not normalized, so take all frames which will be used, compute std and mean and normalize data
+            self.synop_data, synop_mean, synop_std = normalize_synop_data(self.synop_data, synop_data_indices,
+                                                                          list(list(zip(*self.train_params))[1]),
+                                                                          self.sequence_length + self.prediction_offset + self.future_sequence_length)
+            print(synop_mean[target_param_index])
+            print(synop_std[target_param_index])
 
         self.features = [self.synop_data.iloc[index:index + self.sequence_length][list(list(zip(*self.train_params))[1])].to_numpy()
                                 for index in tqdm(synop_data_indices)]