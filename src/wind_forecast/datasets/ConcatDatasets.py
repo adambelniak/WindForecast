9606491cbed6355278b1cd73ee615a08c0438652
MBelniak <rubik40s@wp.pl>
2021-10-01 15:40:14 +0200

    Fix mean and std access in concat dataset

--- src/wind_forecast/datasets/ConcatDatasets.py
+++ src/wind_forecast/datasets/ConcatDatasets.py
@@ -4,6 +4,8 @@ import torch
 class ConcatDatasets(torch.utils.data.Dataset):
     def __init__(self, *datasets):
         self.datasets = datasets
+        self.mean = [dataset.mean for dataset in datasets]
+        self.std = [dataset.std for dataset in datasets]
 
     def __getitem__(self, i):
         return tuple(d[i] for d in self.datasets)
