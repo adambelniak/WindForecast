Fix normalization of gfs

src/wind_forecast/datamodules/Sequence2SequenceDataModule.py
@@ -136,13 +136,14 @@ class Sequence2SequenceDataModule(LightningDataModule):
         self.removed_dataset_indices.extend(next_removed_indices)
 
         if self.gfs_train_params is not None:
-            all_gfs_input_data = [item for index, item in enumerate(all_gfs_input_data) if
-                                  index not in next_removed_indices]
+            all_gfs_input_data = np.array([item for index, item in enumerate(all_gfs_input_data) if
+                                  index not in next_removed_indices])
+            all_gfs_input_data = normalize_gfs_data(all_gfs_input_data, self.normalization_type, (0, 1))
 
         if self.target_param == "wind_velocity":
             gfs_target_data = np.array([math.sqrt(velocity[0] ** 2 + velocity[1] ** 2) for velocity in gfs_target_data])
 
-        gfs_target_data = normalize_gfs_data(gfs_target_data, self.normalization_type)
+        gfs_target_data = normalize_gfs_data(gfs_target_data, self.normalization_type, (0, 1))
 
         assert len(self.synop_data_indices) == len(gfs_target_data)
 

src/wind_forecast/util/gfs_util.py
@@ -209,12 +209,12 @@ def initialize_GFS_date_keys_for_sequence(date_keys: [str], labels: pd.DataFrame
     return new_list
 
 
-def normalize_gfs_data(gfs_data: np.ndarray, normalization_type: NormalizationType):
+def normalize_gfs_data(gfs_data: np.ndarray, normalization_type: NormalizationType, axes=-1):
     if normalization_type == NormalizationType.STANDARD:
-        return (gfs_data - np.mean(gfs_data)) / np.std(gfs_data)
+        return (gfs_data - np.mean(gfs_data, axis=axes)) / np.std(gfs_data, axis=axes)
     else:
-        return (gfs_data - np.min(gfs_data)) / (
-                    np.max(gfs_data) - np.min(gfs_data))
+        return (gfs_data - np.min(gfs_data, axis=axes)) / (
+                    np.max(gfs_data, axis=axes) - np.min(gfs_data, axis=axes))
 
 
 def get_nearest_lat_lon_from_coords(gfs_coords: [[float]], original_coords: Coords):