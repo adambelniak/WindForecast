768d1465cb2228b5801d7aeb1d6d77f7e173ccea
MBelniak <rubik40s@wp.pl>
2021-11-11 17:40:43 +0100

    Put dates after input embeddings

--- src/wind_forecast/models/TransformerEncoderS2SWithGFS.py
+++ src/wind_forecast/models/TransformerEncoderS2SWithGFS.py
@@ -18,8 +18,7 @@ class TransformerEncoderS2SWithGFS(TransformerBaseProps):
             self.embed_dim += gfs_params_len * (config.experiment.time2vec_embedding_size + 1)
 
         if config.experiment.with_dates_inputs:
-            input_features_length += 2
-            self.embed_dim += 2 * (config.experiment.time2vec_embedding_size + 1)
+            self.embed_dim += 2
 
         self.time_2_vec_time_distributed = TimeDistributed(Time2Vec(input_features_length,
                                                                     config.experiment.time2vec_embedding_size), batch_first=True)
@@ -45,19 +44,17 @@ class TransformerEncoderS2SWithGFS(TransformerBaseProps):
     def forward(self, inputs, gfs_inputs, gfs_targets, targets: torch.Tensor, epoch: int, stage=None,
                 dates_embeddings: (torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor) = None):
         if gfs_inputs is None:
-            if dates_embeddings is None:
-                x = [inputs]
-            else:
-                x = [inputs, dates_embeddings[0], dates_embeddings[1]]
-            time_embedding = torch.cat([*x, self.time_2_vec_time_distributed(x)], dim=-1)
+            time_embedding = torch.cat([inputs, self.time_2_vec_time_distributed(inputs)], dim=-1)
         else:
-            if dates_embeddings is None:
-                x = [inputs, gfs_inputs]
-            else:
-                x = [inputs, gfs_inputs, dates_embeddings[0], dates_embeddings[1]]
-            time_embedding = torch.cat([*x,
-                                        self.time_2_vec_time_distributed(torch.cat(x, dim=-1))], dim=-1)
-        x = self.pos_encoder(time_embedding) if self.use_pos_encoding else time_embedding
+            x = [inputs, gfs_inputs]
+            time_embedding = torch.cat([*x, self.time_2_vec_time_distributed(torch.cat(x, dim=-1))], dim=-1)
+
+        if dates_embeddings is None:
+            x = time_embedding
+        else:
+            x = torch.cat([time_embedding, dates_embeddings[0], dates_embeddings[1]], -1)
+
+        x = self.pos_encoder(x) if self.use_pos_encoding else x
         x = self.encoder(x)
 
         if dates_embeddings is None:
