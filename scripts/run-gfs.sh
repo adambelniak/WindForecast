8ea182f3be80b8024a5fb956d6e63562c7fca60f
MBelniak <rubik40s@wp.pl>
2022-09-18 18:10:01 +0200

    Fix newlines in sh

--- scripts/run-gfs.sh
+++ scripts/run-gfs.sh
@@ -64,157 +64,157 @@ experiment.regressor_head_dims=[64,128,32] experiment.epochs=20 experiment.num_w
 #BiLSTM
 python -m wind_forecast.main experiment=hybrid_bi_lstm_s2s_gfs optim=adam experiment.target_parameter=temperature \
 experiment.dropout=0.2 optim.base_lr=0.0003 experiment.lstm_hidden_state=256 experiment.lstm_num_layers=4 \
-experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2
+experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2 \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=hybrid_bi_lstm_s2s_gfs optim=adam experiment.target_parameter=temperature \
 experiment.dropout=0.2 optim.base_lr=0.0003 experiment.lstm_hidden_state=256 experiment.lstm_num_layers=4 \
-experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2
+experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2 \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 python -m wind_forecast.main experiment=hybrid_bi_lstm_s2s_gfs optim=adam experiment.target_parameter=wind_velocity \
 experiment.dropout=0.2 optim.base_lr=0.0003 experiment.lstm_hidden_state=256 experiment.lstm_num_layers=4 \
-experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2
+experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2 \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=hybrid_bi_lstm_s2s_gfs optim=adam experiment.target_parameter=wind_velocity \
 experiment.dropout=0.2 optim.base_lr=0.0003 experiment.lstm_hidden_state=256 experiment.lstm_num_layers=4 \
-experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2
+experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2 \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 python -m wind_forecast.main experiment=hybrid_bi_lstm_s2s_gfs optim=adam experiment.target_parameter=pressure \
 experiment.dropout=0.2 optim.base_lr=0.0003 experiment.lstm_hidden_state=256 experiment.lstm_num_layers=4 \
-experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2
+experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2 \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=hybrid_bi_lstm_s2s_gfs optim=adam experiment.target_parameter=pressure \
 experiment.dropout=0.2 optim.base_lr=0.0003 experiment.lstm_hidden_state=256 experiment.lstm_num_layers=4 \
-experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2
+experiment.use_time2vec=False experiment.use_value2vec=True experiment.value2vec_embedding_factor=2 \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 #TCN Encoder
 python -m wind_forecast.main experiment=tcn_encoder_s2s_gfs optim=adam experiment.target_parameter=temperature \
 experiment.dropout=0.3 optim.base_lr=0.0001 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64] \
 experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=tcn_encoder_s2s_gfs optim=adam experiment.target_parameter=temperature \
 experiment.dropout=0.3 optim.base_lr=0.0001 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64] \
 experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 python -m wind_forecast.main experiment=tcn_encoder_s2s_gfs optim=adam experiment.target_parameter=wind_velocity \
 experiment.dropout=0.3 optim.base_lr=0.0001 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64] \
 experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=tcn_encoder_s2s_gfs optim=adam experiment.target_parameter=wind_velocity \
 experiment.dropout=0.3 optim.base_lr=0.0001 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64] \
 experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 python -m wind_forecast.main experiment=tcn_encoder_s2s_gfs optim=adam experiment.target_parameter=pressure \
 experiment.dropout=0.3 optim.base_lr=0.0001 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64] \
 experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=tcn_encoder_s2s_gfs optim=adam experiment.target_parameter=pressure \
 experiment.dropout=0.3 optim.base_lr=0.0001 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[128,128,64] \
 experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 # TCN
 python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs optim=adam experiment.target_parameter=temperature \
 experiment.dropout=0.05 optim.base_lr=0.0001 experiment.tcn_kernel_size=2 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32] \
 experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs optim=adam experiment.target_parameter=temperature \
 experiment.dropout=0.05 optim.base_lr=0.0001 experiment.tcn_kernel_size=2 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32] \
 experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs optim=adam experiment.target_parameter=wind_velocity \
 experiment.dropout=0.05 optim.base_lr=0.0001 experiment.tcn_kernel_size=2 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32] \
 experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs optim=adam experiment.target_parameter=wind_velocity \
 experiment.dropout=0.05 optim.base_lr=0.0001 experiment.tcn_kernel_size=2 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32] \
 experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs optim=adam experiment.target_parameter=pressure \
 experiment.dropout=0.05 optim.base_lr=0.0001 experiment.tcn_kernel_size=2 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32] \
 experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs optim=adam experiment.target_parameter=pressure \
 experiment.dropout=0.05 optim.base_lr=0.0001 experiment.tcn_kernel_size=2 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[64,32] \
 experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 # TCN Attention
 python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs_attention optim=adam experiment.target_parameter=temperature \
 experiment.dropout=0.6 optim.base_lr=0.0002 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32] \
  experiment.epochs=20 experiment.num_workers=16
 
  python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs_attention optim=adam experiment.target_parameter=temperature \
 experiment.dropout=0.6 optim.base_lr=0.0002 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32] \
  experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
   python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs_attention optim=adam experiment.target_parameter=wind_velocity \
 experiment.dropout=0.6 optim.base_lr=0.0002 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32] \
  experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
   python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs_attention optim=adam experiment.target_parameter=wind_velocity \
 experiment.dropout=0.6 optim.base_lr=0.0002 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32] \
  experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
   python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs_attention optim=adam experiment.target_parameter=pressure \
 experiment.dropout=0.6 optim.base_lr=0.0002 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32] \
  experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
   python -m wind_forecast.main experiment=hybrid_tcn_s2s_gfs_attention optim=adam experiment.target_parameter=pressure \
 experiment.dropout=0.6 optim.base_lr=0.0002 experiment.tcn_kernel_size=3 experiment.tcn_channels=[32,64,64] \
-experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32]
+experiment.use_time2vec=False experiment.use_value2vec=False experiment.regressor_head_dims=[32] \
  experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 
 # Transformer Encoder
 python -m wind_forecast.main experiment=transformer_encoder_s2s_gfs optim=adam experiment.target_parameter=temperature \
 experiment.dropout=0.7 optim.base_lr=0.008 experiment.transformer_ff_dim=512 \
-experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False
+experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=transformer_encoder_s2s_gfs optim=adam experiment.target_parameter=temperature \
 experiment.dropout=0.7 optim.base_lr=0.008 experiment.transformer_ff_dim=512 \
-experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False
+experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 python -m wind_forecast.main experiment=transformer_encoder_s2s_gfs optim=adam experiment.target_parameter=wind_velocity \
 experiment.dropout=0.7 optim.base_lr=0.008 experiment.transformer_ff_dim=512 \
-experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False
+experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=transformer_encoder_s2s_gfs optim=adam experiment.target_parameter=wind_velocity \
 experiment.dropout=0.7 optim.base_lr=0.008 experiment.transformer_ff_dim=512 \
-experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False
+experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 python -m wind_forecast.main experiment=transformer_encoder_s2s_gfs optim=adam experiment.target_parameter=pressure \
 experiment.dropout=0.7 optim.base_lr=0.008 experiment.transformer_ff_dim=512 \
-experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False
+experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16
 
 python -m wind_forecast.main experiment=transformer_encoder_s2s_gfs optim=adam experiment.target_parameter=pressure \
 experiment.dropout=0.7 optim.base_lr=0.008 experiment.transformer_ff_dim=512 \
-experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False
+experiment.transformer_encoder_layers=6 experiment.use_time2vec=False experiment.use_value2vec=False \
 experiment.regressor_head_dims=[64,32] experiment.epochs=20 experiment.num_workers=16 experiment.sequence_length=48
 
 
